c8b1e81c6ccd9d85e1a55493b3f4ebfb9845d584
chamaboy <hiroponpoko07@gmail.com>
2020-04-14 15:24:53 +0900

    feat(firebase): AngularとFirebaseの連携

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCnjyxEI00EiMp31R6JlZ0pfa552r9kMzw',
+    authDomain: 'gitpet-297a5.firebaseapp.com',
+    databaseURL: 'https://gitpet-297a5.firebaseio.com',
+    projectId: 'gitpet-297a5',
+    storageBucket: 'gitpet-297a5.appspot.com',
+    messagingSenderId: '618021870691',
+    appId: '1:618021870691:web:03f375efd0c37b3848ac56',
+    measurementId: 'G-EK419WFRBN'
+  }
 };
 
 /*

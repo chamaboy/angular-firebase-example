a1fe6a7a3db491019f5ffaf0a9613c7a54ac477a
chamaboy <hiroponpoko07@gmail.com>
2020-04-15 17:55:08 +0900

    fix(guest-guard):  リダイレクト処理修正

--- src/app/guards/guest.guard.ts
+++ src/app/guards/guest.guard.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { map, take, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GuestGuard implements CanActivate, CanLoad {
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     return this.authService.afUser$.pipe(
-      map(user => !user),
-      tap(isGuest => {
+      map((user) => !user),
+      tap((isGuest) => {
         if (!isGuest) {
           this.router.navigateByUrl('/');
         }
@@ -26,10 +37,16 @@ export class GuestGuard implements CanActivate, CanLoad {
   }
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.afUser$.pipe(
-      map(user => !user),
-      take(1)
+      map((user) => !user),
+      take(1),
+      tap((isGuest) => {
+        if (!isGuest) {
+          this.router.navigateByUrl('/');
+        }
+      })
     );
   }
 }

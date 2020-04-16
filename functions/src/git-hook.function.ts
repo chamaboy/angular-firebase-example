5735c70920a8f01cde08e0638943f8c7427c7362
chamaboy <hiroponpoko07@gmail.com>
2020-04-16 14:06:24 +0900

    feat(gitHook):  経験値データの反映

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -1,7 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const db = admin.firestore();
 
 export const gitHook = functions
   .region('asia-northeast1')
-  .https.onRequest((request, response) => {
+  .https.onRequest(async (request, response) => {
+    const pets = await db
+      .collection('pets')
+      .where('owenerGitHubId', '==', request.body.sender.id)
+      .get();
+    const increment = admin.firestore.FieldValue.increment(10);
+    pets.docs.forEach((doc) =>
+      doc.ref.update({
+        exp: increment,
+      })
+    );
+
     response.send('Success!');
   });
--- src/app/create/create/create.component.ts
+++ src/app/create/create/create.component.ts
@@ -51,6 +51,7 @@ export class CreateComponent implements OnInit {
       level: 1,
       exp: 0,
       trainerId: this.authService.uid,
+      ownerGitHubId: this.authService.gitHubId,
     });
   }
 }
